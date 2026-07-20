fa8fde072d579a023652adae8b1a2fcd2cc5ab87
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2026-07-20 23:07:32 +0600

    change background design

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,6 +10,13 @@ export default function Home() {
   return (
     <>
       <div className="relative min-h-screen overflow-hidden bg-linear-to-br/decreasing from-[#0F172A] via-[#111827] to-[#2563EB]/20">
+        <div className="absolute inset-0 pointer-events-none z-0">
+          <div className="absolute top-0 left-0 right-0 h-px " />
+          <div className="absolute -top-50 left-1/2 -translate-x-1/2 w-200 h-100  blur-[120px] rounded-full" />
+          <div className="absolute bottom-12 left-10 w-72 h-72  blur-[80px] rounded-full" />
+          <div className="absolute bottom-20 right-10 w-80 h-80  rounded-full" />
+          <div className="absolute inset-0 bg-[linear-gradient(to_right,#1f29370b_1px,transparent_1px),linear-gradient(to_bottom,#1f29370b_1px,transparent_1px)] bg-size-[4rem_4rem]" />
+        </div>
         <Navbar />
         <Hero data={heroSectionData as any} />
         <MySkills />
--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -23,17 +23,6 @@ export default function Footer() {
   return (
     <footer className="relative overflow-hidden  px-6 pt-20 pb-12 text-slate-400 md:px-12 lg:px-24">
       {/* Premium Background Layers */}
-      <div className="absolute inset-0 pointer-events-none z-0">
-        {/* Top Border Gradient Glow */}
-        <div className="absolute top-0 left-0 right-0 h-px " />
-        {/* Soft Radial Neon Glow */}
-        <div className="absolute -top-50 left-1/2 -translate-x-1/2 w-200 h-100  blur-[120px] rounded-full" />
-        {/* Secondary Blur Circles */}
-        <div className="absolute bottom-12 left-10 w-72 h-72 bg-blue-500/3 blur-[80px] rounded-full" />
-        <div className="absolute bottom-20 right-10 w-80 h-80 bg-purple-500/3 blur-[90px] rounded-full" />
-        {/* Fine Tech Grid overlay */}
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#1f29370b_1px,transparent_1px),linear-gradient(to_bottom,#1f29370b_1px,transparent_1px)] bg-[size:4rem_4rem]" />
-      </div>
 
       <motion.div
         className="relative z-10 mx-auto max-w-7xl"
@@ -50,14 +39,14 @@ export default function Footer() {
             availability={footerData.availability}
           />
           <FooterNavigation links={footerData.navigationLinks} />
-          <FooterContact items={footerData.contactInfo} />
+          {/* <FooterContact items={footerData.contactInfo} /> */}
         </div>
 
         {/* Dynamic Social & CTA Middle Row Layout */}
-        <div className="mt-16 flex flex-col items-center justify-between gap-8 border-t border-slate-800/60 pt-12 lg:flex-row">
+        {/* <div className="mt-16 flex flex-col items-center justify-between gap-8 border-t border-slate-800/60 pt-12 lg:flex-row">
           <FooterSocial socials={footerData.socialLinks} />
           <FooterCTA />
-        </div>
+        </div> */}
 
         {/* Bottom Metadata & Legal Bar */}
         <FooterBottom name={footerData.name} techStack={footerData.techStack} />
